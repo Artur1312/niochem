73a2b2c9181069570e8736546486a292888bef88
Artur1312 <arturplay007@gmail.com>
2017-07-27 14:20:31 +0300

    auth

---
+++ controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mint2
+ * Date: 27.07.17
+ * Time: 11:45
+ */
+
+namespace app\controllers;
+
+
+use app\models\LoginForm;
+use app\models\User;
+use Yii;
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return \yii\web\Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+//    public function actionValid()
+//    {
+//        $user = User::findOne(1);
+//        Yii::$app->user->login($user);
+//
+//        if(Yii::$app->user->isGuest)
+//        {
+//            echo 'guest';
+//        }else{
+//            echo 'authorized';
+//        }
+//    }
+
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,6 +5,7 @@
 
 use app\assets\PublicAsset;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
@@ -52,8 +53,18 @@ PublicAsset::register($this);
                 </ul>
                 <div class="i_con">
                     <ul class="nav navbar-nav text-uppercase">
-                        <li><a href="/login">Login</a></li>
-                        <li><a href="register">Register</a></li>
+                        <?php if(Yii::$app->user->isGuest):?>
+                            <li><a href="<?= Url::toRoute(['auth/login'])?>">Login</a></li>
+                            <li><a href="<?= Url::toRoute(['auth/signup'])?>">Register</a></li>
+                        <?php else: ?>
+                            <li><a href="<?= Url::toRoute(['/auth/logout'])?>">Logout(<?=Yii::$app->user->identity->name?>)</a></li>
+<!--                           Html::beginForm(['/auth/logout'], 'post')-->
+<!--                            . Html::submitButton(-->
+<!--                                'Logout (' . Yii::$app->user->identity->name . ')',-->
+<!--                                ['class' => 'btn btn-link logout', 'style'=>"padding-top:10px;"]-->
+<!--                            )-->
+<!--                            . Html::endForm() -->
+                        <?php endif;?>
                     </ul>
                 </div>
 
